705181de46f70a101690f78a74130aa51ff3e0ce
unknown <U0160321@U0160321-TPD-A.ten.thomsonreuters.com>
2014-05-09 14:48:02 -0400

    Major update of content and images + style.

--- Rolodex/App/Controllers/WorkController.js
+++ Rolodex/App/Controllers/WorkController.js
@@ -3,14 +3,24 @@
 
     	$scope.sortWorkOrder = "-eYear";
 
-	    $scope.aboutDetails = "true";
+    	$scope.aboutDetails = "true";
+    	$scope.aboutPics = "false";
+	    $scope.aboutAll = "false";
 
       $scope.toggleShowWorkDetails = function (work) {
         work.showDetails = !work.showDetails;
       };
 
-      $scope.toggleShowAboutDetails = function (about) {
+      $scope.toggleShowAboutDetails = function () {
       	$scope.aboutDetails = !$scope.aboutDetails;
+      	$scope.aboutAll = $scope.aboutPics && $scope.aboutDetails;
+      };
+
+      $scope.toggleShowAboutPictures = function () {
+      	$scope.aboutPics = !$scope.aboutPics;
+      	$scope.aboutAll = $scope.aboutPics && $scope.aboutDetails;
+      	console.log("aboutPics is ", $scope.aboutPics);
+	      console.log("aboutAll is ", $scope.aboutAll);
       };
 
       $scope.worksites =
@@ -21,31 +31,31 @@
 								2014,
 								"Current",
 								2014,
-								"Lorem Ipsum",
+								"Document Management System Development",
 								"7322 Newman Blvd",
 								"Dexter",
 								"MI",
 								3
 						),
-						new Work("Programming Intern",
+						new Work("Accounting Development Intern",
 								"Thomson Reuters",
 								"May",
 								2013,
 								"August",
 								2013,
-								"Lorem Ipsum",
+								"Workpapers CS/Engagement",
 								"7322 Newman Blvd",
 								"Dexter",
 								"MI",
 								2
 						),
 						new Work("Programming Intern",
-								"Retail Me Not, Inc",
+								"Retail Me Not, Inc.",
 								"May",
 								2012,
 								"August",
 								2012,
-								"Lorem Ipsum",
+								"Web Development",
 								"301 Congress Ave #700",
 								"Austin",
 								"TX",
@@ -57,7 +67,7 @@
 								2011,
 								"May",
 								2012,
-								"Lorem Ipsum",
+								"Software-Defined Radio development, in conjunction with Microsoft Research",
 								"1301 Beal Ave",
 								"Ann Arbor",
 								"MI",
