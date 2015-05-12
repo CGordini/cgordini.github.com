360da5aced5b00dcdc4dc508bf7c09f9e6e25794
Cory Gordinier <cory.gordinier@thomsonreuters.com>
2015-05-12 00:02:05 -0400

    Added Instagram

--- App/Models/badgelist.js
+++ App/Models/badgelist.js
@@ -9,33 +9,37 @@
             var factory = {};
 
             factory.getBadges = function(){
-                var internalList = new Array(7);
+                var internalList = new Array(8);
 
-                internalList[0] = new Badge("My Github",
+                internalList[0] = new Badge("Instagram",
+                                         "https://instagram.com/cgordini/",
+                                         "fa-instagram");
+
+                internalList[1] = new Badge("Github",
                                          "https://github.com/CGordini",
                                          "fa-github");
 
-                internalList[1] = new Badge("My Bitbucket",
+                internalList[2] = new Badge("Bitbucket",
                                          "https://bitbucket.org/cgordini",
                                          "fa-bitbucket");
 
-                internalList[2] = new Badge("My Blog",
+                internalList[3] = new Badge("My Blog",
                                          "http://cgordini.blogspot.com/",
                                          "fa-google");
 
-                internalList[3] = new Badge("My LinkedIn",
+                internalList[4] = new Badge("LinkedIn",
                                          "https://www.linkedin.com/pub/cory-gordinier/4b/288/b07",
                                          "fa-linkedin");
 
-                internalList[4] = new Badge("My Resume",
+                internalList[5] = new Badge("My Resume",
                                          "App/Docs/Resume.pdf",
                                          "fa-file-text");
 
-                internalList[5] = new Badge("My References",
+                internalList[6] = new Badge("My References",
                                          "App/Docs/References.pdf",
                                          "fa-file-pdf-o");
 
-                internalList[6] = new Badge("Email Me",
+                internalList[7] = new Badge("Email Me",
                                          "mailto:cory.gordinier@gmail.com",
                                          "fa-envelope-o");
 
