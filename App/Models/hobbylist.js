b10a9b83494e5b3b7191e685cb86b822b95768ae
Cory Gordinier <cory.gordinier@thomsonreuters.com>
2016-07-24 13:30:28 -0400

    Additions of newest blog posts.

--- App/Models/hobbylist.js
+++ App/Models/hobbylist.js
@@ -9,7 +9,7 @@
             var factory = {};
 
             factory.getSlides = function(){
-                var internalList = new Array(3);
+                var internalList = new Array(5);
 
                 internalList[0] = new Slide("Lights Out in Michigan",
                                             "http://cgordini.kinja.com/lights-out-in-michigan-1783062234",
@@ -21,7 +21,27 @@
                                             "<a href=\"http://cgordini.kinja.com/lights-out-in-michigan-1783062234\">...</a>"
                                         );
 
-                internalList[1] = new Slide("My Car Fax, Part 1",
+                internalList[1] = new Slide("#becauseracecar (July 21st/22nd, 2016)",
+                                            "http://cgordini.kinja.com/becauseracecar-july-21st-22nd-2016-1784215359",
+                                            "July 24th, 2016",
+                                            "https://i.kinja-img.com/gawker-media/image/upload/s--KHYVd8nJ--/c_fit,fl_progressive,q_80,w_636/dkykhyflotmvuikpowf6.jpg",
+                                            "I spent my Friday out doing the Motor State Challenge at Gingerman Raceway, South Haven, MI. This was only my second track day, so clearly I'm an expert now.<br/><br/>" +
+                                            "Also, my first time going the \"right direction\" on the track -- when I previously raced via <a href=\"http://www.autointerest.com/\">AutoInterests</a>, we ran the track counter-clockwise. That time I had a seasoned driving instructor in the car.<br/><br/>" +
+                                            "This time, I had to make up my own lines, which in some areas was happily easy and I could tell when I nailed them. In other areas, not so much" +
+                                            "<a href=\"http://cgordini.kinja.com/becauseracecar-july-21st-22nd-2016-1784215359\">...</a>"
+                                        );
+
+
+                internalList[2] = new Slide("Cars and Coffee (Week of July 10th)",
+                                            "http://cgordini.kinja.com/cars-and-coffee-week-of-july-10th-1783791336",
+                                            "July 16th, 2016",
+                                            "https://i.kinja-img.com/gawker-media/image/upload/s--J9L8h2Bg--/c_fit,fl_progressive,q_80,w_636/l5gop3c494k90mgwego8.jpg",
+                                            "For the last couple months, I've been attending Cars & Coffee(s), along with assorted other shows. Now that I have this blog, I figured I can start actually sharing the results of my adventures.<br/><br/>" +
+                                            "All photos are not edited *except* for some small amounts of rotation (blame the photographer adjusting to a DSLR), and the censoring of any public plates for protection of the owner.<br/><br/>" +
+                                            "<a href=\"https://www.flickr.com/gp/145503767@N06/77h3US\">https://www.flickr.com/gp/145503767@N06/77h3US</a>"
+                                        );
+
+                internalList[3] = new Slide("My Car Fax, Part 1",
                                             "http://cgordini.kinja.com/my-car-fax-1783634243",
                                             "July 14th, 2016",
                                             "https://i.kinja-img.com/gawker-media/image/upload/s--jMwZbWpd--/c_fit,fl_progressive,q_80,w_636/eegssiirfnmk18tpdtv3.jpg",
@@ -31,7 +51,7 @@
                                             "<a href=\"http://cgordini.kinja.com/my-car-fax-1783634243\">...</a>"
                                         );
 
-                internalList[2] = new Slide("My Car Fax, Part 2",
+                internalList[4] = new Slide("My Car Fax, Part 2",
                                             "http://cgordini.kinja.com/my-car-fax-part-2-1783634321",
                                             "July 14th, 2016",
                                             "https://i.kinja-img.com/gawker-media/image/upload/s--kcO_Wdwf--/c_fit,fl_progressive,q_80,w_636/n9ftrvafb5stdqhduhlr.jpg",
