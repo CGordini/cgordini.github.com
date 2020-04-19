3538ed2c97ffce3dee89dd59f726fcea15c9dbcc
CGordini <cgordini@umich.edu>
2020-04-19 18:28:08 -0400

    Deleted old files from MASTER

--- App/Directives/cjgdraggable.js
+++
@@ -1,8 +0,0 @@
-angular.module('app').directive('cjgdraggable', function() {
-    return {
-        restrict:'A',
-        link: function(scope, element, attrs) {
-            element.draggable();
-        }
-    };
-});
